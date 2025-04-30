0c3f32c6511b72460307626b1747d019a7c99004
evstalk <elliottviveiros@gmail.com>
2025-04-30 14:33:28 -0700

    uploaded thingies and stuff
    woo hoo ! ! !

---
+++ thingymajiggy/app.js
@@ -0,0 +1,17 @@
+
+
+window.addEventListener('DOMContentLoaded', function () {
+
+    var holder = document.querySelector(".first");
+    var image = document.querySelector(".image");
+    var titleThings = document.querySelector(".titlethingy");
+    var one = document.querySelector(".para1");
+    var two = document.querySelector(".para2");
+    holder.classList.add("container");
+    image.classList.add("thecontained");
+    titleThings.classList.add("title");
+    one.classList.add("para");
+    two.classList.add("para");
+
+
+});
